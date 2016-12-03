67dcb4dd3cedd75a3eeed13587f57171c165e9b9
Arctic Ice Studio <development@arcticicestudio.com>
2016-12-03 11:05:06 +0100

    GHI-#7 Update public API JavaDoc to match Nord color palette docs

--- src/main/java/com/arcticicestudio/nord/package-info.java
+++ src/main/java/com/arcticicestudio/nord/package-info.java
@@ -2,7 +2,6 @@
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 title      Nord API Package Info                        +
 project    nord-java                                    +
-version    0.1.0                                        +
 repository https://github.com/arcticicestudio/nord-java +
 author     Arctic Ice Studio                            +
 email      development@arcticicestudio.com              +
@@ -10,16 +9,13 @@ copyright  Copyright (C) 2016                           +
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 */
 /**
- * Public API of the <a href="https://github.com/arcticicestudio/nord-java">Nord - Java</a> project, which implements
+ * Public API of the <a href="https://github.com/arcticicestudio/nord-java">Nord Java</a> project, which implements
  * the <a href="https://github.com/arcticicestudio/nord">Nord</a> color palette.
  *
  * <p>
- *   The main entry point of the API is the {@link com.arcticicestudio.nord.Nord} enum.
- * </p>
+ *   The public API entry point is the {@link com.arcticicestudio.nord.Nord} enum.
  *
  * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
- * @see <a href="https://github.com/arcticicestudio/nord-java">Nord - Java </a>
- * @see <a href="https://github.com/arcticicestudio/nord">Nord</a>
  * @version 0.1.0
  * @since 0.1.0
  */
